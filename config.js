f838b6aec6f397566dee083861202fda8e4bbba0
Aspect Devlopment <93891805+AspectDevlopment@users.noreply.github.com>
2021-11-15 22:43:49 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+// Site Settings //
+
+var siteTitle = `JextheDev`;
+var siteDescription = "Hey My Name i s Jex and i am learning how to Dev on FiveM servers in LUA,JS and Hosting Fivem Server";
+var buttonColor = "#000aff";
+var buttonHoverColor = "#ffffff";
+
+var firstBoxGradColor = "#1546ff";
+var secondBoxGradColor = "#ffffff";
+
+// Buttons //
+// Isons can range from anything in /assets/icons.svg. Like youtube, discord, twitter, github, globe etc.
+var buttonsArray = [
+    // Format: "LINK::Icon"
+    // "http://example.com::twitter"
+    "https://discord.gg/eBFtyxyV9F::discord",
+    "https://www.twitch.tv/trx_jexzu::globe",
+    "http://betterruger@gmail.com::globe",
+]
